ea57bf8033bb33be95245500d866f64f2a667024
svanholm200-debug <svanholm200@gmail.com>
2026-02-23 09:43:43 +0100

    first commit

---
+++ src/main/java/com/example/helloworldgithubactions/HelloWorldGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldGitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldgithubactions/HelloWorldGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
